05ab240509ab7bc21efeddf03979196e4c00deab
Ubuntu <ubuntu@ip-172-31-21-31.us-east-2.compute.internal>
2025-08-02 22:30:47 +0000

    Atualização do EC2 com versão de produção (build, configs e ajustes)

---
+++ frontend/src/config/api.js
@@ -0,0 +1,2 @@
+const API_BASE_URL = "http://3.145.29.139:5000/api"; // ou apenas http://3.145.29.139:5000 se não usar prefixo /api
+export default API_BASE_URL;
--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -1,11 +1,11 @@
+// vite.config.js ou vite.config.mjs, dependendo da sua escolha
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
